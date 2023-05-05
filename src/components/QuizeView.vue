0abb5d239608ce317bdb2c4b68f3eaf7e2315048
iraklikeche <irakli.keche@yahoo.com>
2023-05-05 16:21:58 +0400

    quiz_version_fnd

--- src/components/QuizeView.vue
+++ src/components/QuizeView.vue
@@ -4,25 +4,55 @@ import { useRoute } from "vue-router";
 import quiz from "../data/quizes.json";
 import Question from "./Question.vue";
 import QuizHeader from "./QuizHeader.vue";
+import Result from "./Result.vue";
 
 const route = useRoute();
 
 const quize = quiz.find((q) => q.id === parseInt(route.params.id));
 
 const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
 
 const questionStatus = computed(() => {
   return `${currentQuestionIndex.value}/${quize.questions.length}`;
 });
+
+const barPercentage = computed(() => {
+  return `${(currentQuestionIndex.value / quize.questions.length) * 100}%`;
+});
+
+const onOptionSelected = (isCorrect) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++;
+  }
+
+  if (quize.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+
+  currentQuestionIndex.value++;
+};
 </script>
 
 <template>
   <div>
-    <QuizHeader :questionStatus="questionStatus" />
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
     <div>
-      <Question :question="quize.questions[currentQuestionIndex]" />
+      <Question
+        v-if="!showResults"
+        :question="quize.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+      <Result
+        v-else
+        :quezQuestionLength="quize.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      />
     </div>
-    <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
 
---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
+
+const { quezQuestionLength, numberOfCorrectAnswers } = defineProps([
+  "numberOfCorrectAnswers",
+  "quezQuestionLength",
+]);
+</script>
+
+<template>
+  <div class="result">
+    <p>Your Results...</p>
+    <h1>{{ numberOfCorrectAnswers }}/{{ quezQuestionLength }}</h1>
+    <RouterLink to="/">Go Back</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.result {
+  text-align: center;
+  padding: 100px 0;
+}
+p {
+  font-size: 25px;
+}
+
+h1 {
+  font-size: 80px;
+}
+</style>
